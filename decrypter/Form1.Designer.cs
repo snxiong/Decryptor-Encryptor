c0650bf19e00951c580e3c99a4e1b9cb81e6c1d8
Steven Xiong <Steven.Xiong@calpia.ca.gov>
2021-10-12 16:13:46 -0700

    Added async and parallel programming to increase efficiency

--- decrypter/Form1.Designer.cs
+++ decrypter/Form1.Designer.cs
@@ -34,20 +34,25 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(102, 265);
+            this.textBox2.Location = new System.Drawing.Point(28, 269);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(340, 26);
             this.textBox2.TabIndex = 2;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(167, 105);
+            this.button4.Location = new System.Drawing.Point(632, 99);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(87, 65);
+            this.button4.Size = new System.Drawing.Size(125, 125);
             this.button4.TabIndex = 5;
             this.button4.Text = "Load File";
             this.button4.UseVisualStyleBackColor = true;
@@ -55,43 +60,94 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(102, 220);
+            this.textBox1.Location = new System.Drawing.Point(28, 163);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(340, 26);
             this.textBox1.TabIndex = 6;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(303, 105);
+            this.button1.Location = new System.Drawing.Point(632, 269);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(87, 65);
+            this.button1.Size = new System.Drawing.Size(125, 125);
             this.button1.TabIndex = 7;
-            this.button1.Text = "Decrrypt File";
+            this.button1.Text = "Decrypt File";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(607, 280);
+            this.button2.Location = new System.Drawing.Point(453, 269);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(87, 65);
+            this.button2.Size = new System.Drawing.Size(125, 125);
             this.button2.TabIndex = 8;
             this.button2.Text = "Decrypt";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(607, 180);
+            this.button3.Location = new System.Drawing.Point(453, 99);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(87, 65);
+            this.button3.Size = new System.Drawing.Size(125, 125);
             this.button3.TabIndex = 9;
             this.button3.Text = "Encrypt";
             this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(452, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 29);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "TESTING";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(627, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 50);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "File Decrypt \r\nActions";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(28, 368);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 26);
+            this.textBox3.TabIndex = 12;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(228, 368);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(140, 26);
+            this.textBox4.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(224, 345);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 22);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Status";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -112,6 +168,11 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- decrypter/Form1.cs
+++ decrypter/Form1.cs
@@ -14,14 +14,18 @@ using System.IO;
 using Excel = Microsoft.Office.Interop.Excel;
 using System.Windows.Forms;
 using System.Linq;
+using System.Diagnostics;
 
 namespace decrypter
 {
+    
+
     public partial class Form1 : Form
     {
 
         private string anotherKey = "abcdefghijklmnop";
         private string filePath = ""; // empty string
+        private bool stuff = true;
 
 
         public Form1()
@@ -34,11 +38,202 @@ namespace decrypter
             button2.Click += new EventHandler(button2_Click);
 
             button3.Click += new EventHandler(button3_Click);
+
+            
             
         }
 
-        private void button1_Click(object sender, EventArgs e)
+
+        private void decryptStuffAsync()
         {
+
+        }
+        // INFO https://www.youtube.com/watch?v=2moh18sh5p4
+        // File decryption using Asynchronous programming
+        private async Task button1_ClickAsync()
+        {
+            // decrypt_file button
+
+            var sw = new Stopwatch();
+            sw.Start();
+
+            textBox2.Text = "";
+            textBox4.Text = "Processing. . . . .";
+             
+
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlWorkSheet;
+            // Excel.Range range;
+
+            xlWorkBook = xlApp.Workbooks.Open(filePath);
+            xlWorkSheet = xlWorkBook.Worksheets[1];
+
+            //in what row and columnm to begin decrypting on
+            int i = 2; //row
+            int j = 2; //column
+
+            string decryptedData = "";
+
+
+            Excel.Application excelApp = new Excel.Application();
+            Excel.Workbook newXLWorkbook = excelApp.Workbooks.Add();
+            Excel.Worksheet newXLWorksheet = (Excel.Worksheet)newXLWorkbook.Sheets.Add();
+
+            //in what row and column to where to place the decypted file on the new excel file
+            int row = 1;
+            int column = 1;
+
+            
+            do
+            {
+                if (xlWorkSheet.Cells[i, j].Text != "NULL")
+                {
+                    decryptedData = xlWorkSheet.Cells[i, j].Value2;
+                    decryptedData = await Task.Run(() => DecryptString(decryptedData));
+                    newXLWorksheet.Cells[row, column] = decryptedData;
+                    row++;
+                }
+                else
+                {
+                    decryptedData = "NULL";
+                    newXLWorksheet.Cells[row, column] = decryptedData;
+                    row++;
+                }
+
+
+                i++;
+
+            } while (xlWorkSheet.Cells[i, j].Text != string.Empty);
+            
+
+            // location and file name of where the program will save the decypted information
+            excelApp.ActiveWorkbook.SaveAs(@"C:\Users\sxiong\desktop\decrypttest.xls", Excel.XlFileFormat.xlWorkbookNormal);
+
+            newXLWorkbook.Close();
+            excelApp.Quit();
+
+            System.Runtime.InteropServices.Marshal.FinalReleaseComObject(newXLWorksheet);
+            System.Runtime.InteropServices.Marshal.FinalReleaseComObject(newXLWorkbook);
+            System.Runtime.InteropServices.Marshal.FinalReleaseComObject(excelApp);
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            textBox2.Text = "Decryption Done";
+
+            textBox3.Text = sw.ElapsedMilliseconds.ToString();
+
+            textBox4.Text = "Done";
+        }
+
+
+        // File decryption using Parallel programming
+        private async Task button1_ClickParallel()
+        {
+            // decrypt_file button
+
+            var sw = new Stopwatch();
+            sw.Start();
+
+            textBox4.Text = "Processing. . . . .";
+
+
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlWorkSheet;
+            // Excel.Range range;
+
+            xlWorkBook = xlApp.Workbooks.Open(filePath);
+            xlWorkSheet = xlWorkBook.Worksheets[1];
+
+            //in what row and columnm to begin decrypting on
+            int i = 2; //row
+            int j = 2; //column
+
+            string decryptedData = "";
+
+
+            Excel.Application excelApp = new Excel.Application();
+            Excel.Workbook newXLWorkbook = excelApp.Workbooks.Add();
+            Excel.Worksheet newXLWorksheet = (Excel.Worksheet)newXLWorkbook.Sheets.Add();
+
+            //in what row and column to where to place the decypted file on the new excel file
+            int row = 1;
+            int column = 1;
+
+            List<Task<string>> task = new List<Task<string>>();
+
+            do
+            {
+                if (xlWorkSheet.Cells[i, j].Text != "NULL")
+                {
+                    decryptedData = xlWorkSheet.Cells[i, j].Value2;
+                    //decryptedData = await Task.Run(() => DecryptString(decryptedData));
+                    task.Add(Task.Run(() => DecryptString(decryptedData)));
+                    //newXLWorksheet.Cells[row, column] = decryptedData;
+                    //row++;
+                }
+                else
+                {
+                    decryptedData = "NULL";
+                    task.Add(Task.Run(() => DecryptString(decryptedData)));
+                    //newXLWorksheet.Cells[row, column] = decryptedData;
+                    //row++;
+                }
+
+                //newXLWorksheet.Cells[row, column] = decryptedData;
+                //row++;
+
+                i++;
+
+            } while (xlWorkSheet.Cells[i, j].Text != string.Empty);
+
+            var results = await Task.WhenAll(task);
+            
+            foreach(var item in results)
+            {
+                newXLWorksheet.Cells[row, column] = item;
+                row++;
+            }
+            
+
+
+            // location and file name of where the program will save the decypted information
+            excelApp.ActiveWorkbook.SaveAs(@"C:\Users\sxiong\desktop\decrypttest.xls", Excel.XlFileFormat.xlWorkbookNormal);
+
+            newXLWorkbook.Close();
+            excelApp.Quit();
+
+            System.Runtime.InteropServices.Marshal.FinalReleaseComObject(newXLWorksheet);
+            System.Runtime.InteropServices.Marshal.FinalReleaseComObject(newXLWorkbook);
+            System.Runtime.InteropServices.Marshal.FinalReleaseComObject(excelApp);
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            textBox2.Text = "Decryption Done";
+
+            textBox3.Text = sw.ElapsedMilliseconds.ToString();
+
+            textBox4.Text = "Done";
+        }
+
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            await button1_ClickAsync();
+        }
+
+        // File decryption using synchronous programming
+        private async void button1_ClickSynchronous(object sender, EventArgs e)
+        {   // decrypt_file button
+
+            //await button1_ClickAsync();
+
+            textBox4.Text = "Processing. . . . .";
+
+            var sw = new Stopwatch();
+            sw.Start();
+
             Excel.Application xlApp = new Excel.Application();
             Excel.Workbook xlWorkBook;
             Excel.Worksheet xlWorkSheet;
@@ -47,8 +242,9 @@ namespace decrypter
             xlWorkBook = xlApp.Workbooks.Open(filePath);
             xlWorkSheet = xlWorkBook.Worksheets[1];
 
+            //in what row and columnm to begin decrypting on
             int i = 2; //row
-            int j = 5; //column
+            int j = 2; //column
 
             string decryptedData = "" ;
 
@@ -57,6 +253,7 @@ namespace decrypter
             Excel.Workbook newXLWorkbook = excelApp.Workbooks.Add();
             Excel.Worksheet newXLWorksheet = (Excel.Worksheet)newXLWorkbook.Sheets.Add();
 
+            //in what row and column to where to place the decypted file on the new excel file
             int row = 1;
             int column = 1;
 
@@ -82,9 +279,6 @@ namespace decrypter
 
             } while (xlWorkSheet.Cells[i, j].Text != string.Empty);
 
-
-           
-
             // location and file name of where the program will save the decypted information
             excelApp.ActiveWorkbook.SaveAs(@"C:\Users\sxiong\desktop\decrypttest.xls", Excel.XlFileFormat.xlWorkbookNormal);
 
@@ -99,11 +293,17 @@ namespace decrypter
 
             textBox2.Text = "Decryption Done";
 
+            textBox3.Text = sw.ElapsedMilliseconds.ToString();
+
+            textBox4.Text = "DONE";
+
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private async void button2_Click(object sender, EventArgs e)
         {
-            textBox2.Text = DecryptString(textBox1.Text);
+            //textBox2.Text = DecryptString(textBox1.Text);
+
+            await button1_ClickAsync();
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -141,12 +341,16 @@ namespace decrypter
         public static string EncryptData(string message)
         {
             string passKey = string.Empty;
+
+            //passKey = "anotherKey";
             passKey = GetKey();
+        
             RijndaelManaged aes256 = new RijndaelManaged();
+            //4qFdu0feymPJkO6aJnFK1IEFgK/BF2EJAq/o9qxQT3Q=
 
             aes256.KeySize = 256;
             aes256.BlockSize = 256;
-            aes256.Padding = PaddingMode.None;
+            aes256.Padding = PaddingMode.PKCS7;
             aes256.Mode = CipherMode.ECB;
             aes256.Key = Encoding.ASCII.GetBytes(passKey);
             aes256.GenerateIV();
@@ -164,19 +368,22 @@ namespace decrypter
             return Convert.ToBase64String(cypherTextBytes);
         }
 
-
-
-
-
         public static string DecryptString(string text)
         {
             string passKey = string.Empty;
+
+            if(text == "NULL")
+            {
+                return "NULL";
+            }
+
             passKey = GetKey();
+
             RijndaelManaged aes256 = new RijndaelManaged();
 
             aes256.KeySize = 256;
             aes256.BlockSize = 256;
-            aes256.Padding = PaddingMode.None;
+            aes256.Padding = PaddingMode.PKCS7;
             aes256.Mode = CipherMode.ECB;
             aes256.Key = Encoding.ASCII.GetBytes(passKey);
             aes256.GenerateIV();
@@ -188,9 +395,7 @@ namespace decrypter
             return Encoding.UTF8.GetString(plainText);
         }
 
-
-
-
+      
 
         public static string GetKey()
         {
@@ -203,7 +408,9 @@ namespace decrypter
             {
                 if (registryKey != null)
                 {
-                    registryValue = registryKey.GetValue("sN").ToString();
+                   
+                    object keyValue = registryKey.GetValue("sN");
+                    registryValue = keyValue.ToString();
                 }
             }
             catch (Exception ex)
@@ -214,5 +421,16 @@ namespace decrypter
             }
             return registryValue;
         }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
